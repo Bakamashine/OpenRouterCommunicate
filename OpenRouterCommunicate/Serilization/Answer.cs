546f5d8268572346816cc5136d38c774745839c9
Bakamashine <surajaaminu09@gmail.com>
2026-01-16 23:39:35 +0300

    Fixed Warnigs

--- OpenRouterCommunicate/Serilization/Answer.cs
+++ OpenRouterCommunicate/Serilization/Answer.cs
@@ -4,24 +4,24 @@ namespace OpenRouterCommunicate.Serilization
 {
     public class ChatCompletionResponse
     {
-        public List<Choice> choices { get; set; }
+        public List<Choice> choices { get; set; } = null!;
         public long created { get; set; }
-        public string model { get; set; }
-        public string @object { get; set; }
-        public Usage usage { get; set; }
+        public string model { get; set; } = null!;
+        public string @object { get; set; } = null!;
+        public Usage usage { get; set; } = null!;
     }
 
     public class Choice
     {
-        public Message message { get; set; }
+        public Message message { get; set; } = null!;
         public int index { get; set; }
-        public string finish_reason { get; set; }
+        public string finish_reason { get; set; } = null!;
     }
 
     public class Message
     {
-        public string content { get; set; }
-        public string role { get; set; }
+        public string content { get; set; } = null!;
+        public string role { get; set; } = null!;
     }
 
     public class Usage
